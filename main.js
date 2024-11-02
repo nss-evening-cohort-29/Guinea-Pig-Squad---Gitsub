b9d13fdb959eedf33fe7bfddc243bd35e3f14571
Tyler-Davenport <tydavenport0415@gmail.com>
2024-11-01 21:22:59 -0500

    structure of overview page is done

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
+const repos = [
+  {
+    id: 0,
+    name: 'example',
+    description: 'This is an example description for a repository',
+    tags: [
+      'example tag 1', 
+      'example tag 2',
+      'example tag 3'
+    ],
+    pinned: true,
+  },
+  {
+    id: 1,
+    name: 'example 2',
+    description: 'This is also an example description for a repository',
+    tags: [
+      'example tag 1', 
+      'example tag 2',
+      'example tag 3'
+    ],
+    pinned: true,
+  },
+  {
+    id: 2,
+    name: 'example 3',
+    description: 'This is a further example description for a repository',
+    tags: [
+      'example tag 1', 
+      'example tag 2',
+      'example tag 3'
+    ],
+    pinned: true,
+  }
+]
 
 const navbar = document.querySelector('#navbar-container');
 const navbarHtml = `
 <ul class="nav nav-underline">
     <li class="nav-item"> 
-      <a class="nav-link active" aria-current="page" href="#">Overview</a>
+      <a class="nav-link active" aria-current="page" href="index.html">Overview</a>
     </li>
     <li class="nav-item">
-    <a class="nav-link" href="#">Repositories</a>
+    <a class="nav-link" href="repos.html">Repositories</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Projects</a>
@@ -17,10 +52,6 @@ const navbarHtml = `
   </ul>
 `
 
-// const cardsOnDom = (array) => { 
-//   let domString = "";
-//   for (const student of array) {
-//     domString += 
 const profile = document.querySelector('#profile-container');
 
 const sampleString = 'WHEEEEEEE'
@@ -44,7 +75,28 @@ const profileHTML = `
     <a href="#" class="card-link">Another link</a>
   </div>
   </div>
- `
+  `
+
+  const pinnedRepos = document.querySelector("#pinnedRepo-container")
+
+ const cardsOnDom = (array) => { 
+  let domString = "";
+  for (const repo of array) {
+    domString += `<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${repo.name}</h5>
+        <p class="card-text">${repo.description}</p>
+        <a href="#" class="btn btn-primary">☆</a>
+      </div>
+    </div>
+  </div>`
+  };
+
+renderToDom("#pinnedRepo-container", domString);
+};
+
 
 const footerEl = document.querySelector('#footerEl-container')
 const footerHTML = `
@@ -65,6 +117,7 @@ const footerHTML = `
     navbar.innerHTML = navbarHtml;
     profile.innerHTML = profileHTML;
     footerEl.innerHTML = footerHTML;
+    cardsOnDom(repos);
   }
 
   startApp();
